72e8e5f9ec3bf06fce2b54447bc6f96fe6b77a48
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-07-25 16:04:03 +0700

    BT1

--- DemoReadFile/src/DemoReadFIle.java
+++ DemoReadFile/src/DemoReadFIle.java
@@ -1,13 +1,21 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class DemoReadFIle {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("DemoReadFile/DucAnh.txt"));
+//        Cách 1:
+//        File
+        FileReader abc = new FileReader("DemoReadFile/DucAnh.txt");
+        BufferedReader reader = new BufferedReader(abc);
         String readLine1 = reader.readLine();
         System.out.println(readLine1);
 
+
+//        Cách 2:
         FileReader reader1 = new FileReader("DemoReadFile/DucAnh.txt");
-        String readLine2 = reader1.read();
-        System.out.println(readLine2);
+        Scanner scanner = new Scanner(reader1);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
--- DemoWriteFile/src/DemoWriteFile.java
+++ DemoWriteFile/src/DemoWriteFile.java
@@ -1,8 +1,11 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Scanner;
 
-public class DemoWriteFile {
-    public static void main(String[] args) {
+public class DemoWriteFile implements Serializable {
+    public static void main(String[] args) throws IOException {
         try {
             FileWriter writer = new FileWriter("E:\\Code\\CodeGym\\Module 2\\CG-M2-B16-IO-TextFile\\DemoWriteFile\\Hello.txt");
             writer.write("HelloWorld");
@@ -10,5 +13,16 @@ public class DemoWriteFile {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        FileWriter writer = new FileWriter("Yolo.txt");
+//        Scanner scanner = new Scanner(System.in);
+//        String bien = "";
+//        int bien1 = 0;
+//        while (bien1 != -1) {
+//            bien1 = scanner.nextInt();
+//            writer.write(bien1);
+//        }
+//        writer.close();
+
     }
 }
